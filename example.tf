767ba430d96fa989151f0f6b023a06b6b7e3033a
Karl Xu <karl.yogi.xu@gmail.com>
2017-10-16 19:33:59 +0800

    create a s3 bucket for the instance to depends on

--- example.tf
+++ example.tf
@@ -5,8 +5,16 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami           = "ami-fba67596"
     instance_type = "t2.micro"
+
+    depends_on    = ["aws_s3_bucket.example"]
 }
 
 resource "aws_eip" "ip" {
     instance = "${aws_instance.example.id}"
 }
+
+resource "aws_s3_bucket" "example" {
+    bucket = "terraform-getting-started-guide-yogiman"
+    acl    = "private"
+}
+
