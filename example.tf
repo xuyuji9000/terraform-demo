5285c6abf56ea65197dc6ac17e231ba26d44c2b3
Karl Xu <karl.yogi.xu@gmail.com>
2017-10-16 19:22:51 +0800

    add eip with dependency on instance

--- example.tf
+++ example.tf
@@ -6,3 +6,7 @@ resource "aws_instance" "example" {
     ami           = "ami-fba67596"
     instance_type = "t2.micro"
 }
+
+resource "aws_eip" "ip" {
+    instance = "${aws_instance.example.id}"
+}
