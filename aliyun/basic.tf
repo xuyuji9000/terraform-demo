2795dba2f78094b5d71a341306eca5b33792d17d
Karl Xu <karl.yogi.xu@gmail.com>
2017-10-21 19:48:53 +0800

    update alicloud security group rule

--- aliyun/basic.tf
+++ aliyun/basic.tf
@@ -41,6 +41,8 @@ resource "alicloud_security_group" "default" {
 resource "alicloud_security_group_rule" "ping" {
     type              = "ingress"
     ip_protocol       = "icmp"
+    policy            = "accept"
+    priority          = 1
     port_range        = "-1/-1"
     security_group_id = "${alicloud_security_group.default.id}"
     cidr_ip           = "0.0.0.0/0"
@@ -50,6 +52,8 @@ resource "alicloud_security_group_rule" "ssh" {
     type              = "ingress"
     ip_protocol       = "tcp"
     port_range        = "22/22"
+    policy            = "accept"
+    priority          = 1
     security_group_id = "${alicloud_security_group.default.id}"
     cidr_ip           = "0.0.0.0/0"
 }
